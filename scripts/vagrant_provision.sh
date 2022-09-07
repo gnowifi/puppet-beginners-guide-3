a6c41f877f636b6f2134931f7485759680e9e3cc
gnowifi <g@nowifi.net>
2022-09-07 05:28:37 -0500

    fix puppet5 version to Xenial

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # source /etc/lsb-release
 # Not needed anymore as we're installing a specific version of Puppet for the purposes of the book
-wget https://apt.puppetlabs.com/puppet5-release-bionic.deb
-dpkg -i puppet5-release-bionic.deb
+wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
+dpkg -i puppet5-release-xenial.deb
 apt-get update
-apt-get -y install git puppet-agent
+apt-get -y install libc6 git puppet-agent
 echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' >/etc/sudoers.d/puppet
 /opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri
 /opt/puppetlabs/puppet/bin/gem install hiera-eyaml-gpg --no-rdoc --no-ri
