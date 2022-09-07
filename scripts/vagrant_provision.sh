186c19fd9be14b813bad20fbbaf95e9059f3aa7b
gnowifi <g@nowifi.net>
2022-09-07 04:08:54 -0500

    update script

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source /etc/lsb-release
-wget https://apt.puppetlabs.com/puppet-release-${DISTRIB_CODENAME}.deb
-dpkg -i puppet-release-${DISTRIB_CODENAME}.deb
+# source /etc/lsb-release  Not needed anymore as we're installing a specific version of Puppet from the book
+wget https://apt.puppetlabs.com/puppet5-release-bionic.deb
+dpkg -i puppet5-release-bionic.deb
 apt-get update
 apt-get -y install git puppet-agent
 echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' >/etc/sudoers.d/puppet
