ee87e3c7f365ec5602629221429982bb9fb5d394
gnowifi <g@nowifi.net>
2022-09-07 05:39:58 -0500

    install r10k manually

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -4,7 +4,8 @@
 wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
 dpkg -i puppet5-release-xenial.deb
 apt-get update
-apt-get -y install libc6 git puppet-agent
+apt-get -y install libc6 git puppet-agent r10k
+# https://github.com/shekharshan/puppet-beginners-guide-3/commit/7db3e0914935974b32b105b38684b4c52c12d01d
 echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' >/etc/sudoers.d/puppet
 /opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri
 /opt/puppetlabs/puppet/bin/gem install hiera-eyaml-gpg --no-rdoc --no-ri
